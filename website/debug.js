e58c72d0cc08be3cc3c42ca1d27d939c055136cf
Conrad Godfrey <conradjgodfrey@gmail.com>
2023-02-11 15:50:14 +0000

    got that colllisssion detectionnnn

---
+++ website/debug.js
@@ -0,0 +1,21 @@
+const debug = document.getElementById('debug');
+function debugLowestFrequency() {
+    const dataArray = new Uint8Array(analyser.frequencyBinCount);
+    analyser.getByteFrequencyData(dataArray);
+    let loudest = 0;
+    let loudest_index = 0;
+    // get max value in array
+    dataArray.forEach((v, i) => {
+        if (v > loudest) {
+            loudest = v;
+            loudest_index = i;
+        }
+    });
+    debug.innerText = `
+    Lowest frequency: ${loudest_index}
+    Loudest frequency: ${loudest}
+    `;
+    volume = loudest;
+    freq = loudest_index;
+    requestAnimationFrame(debugLowestFrequency);
+}
--- website/slope.js
+++ website/slope.js
@@ -15,7 +15,7 @@ let volume = 0;
 analyser.smoothingTimeConstant = 0.99;
 
 // show analyser contents in canvas
-function draw() {
+function draw_slope() {
     const dataArray = new Uint8Array(analyser.frequencyBinCount);
     analyser.getByteFrequencyData(dataArray);
     // clear canvas
@@ -29,7 +29,7 @@ function draw() {
     const bins_shown = (max_freq - min_freq) / bucket_frequency_size;
     const sliceWidth = canvas.width * 1.0 / bins_shown;
     let x = 0;
-    line = []
+    game_state.line = [];
     dataArray.forEach((v, i) => {
         let frequency = i * bucket_frequency_size;
         if (frequency > max_freq || frequency < min_freq) {
@@ -41,36 +41,29 @@ function draw() {
         } else {
             ctx.lineTo(x, y);
         }
+        game_state.line.push([x,y])
         x += sliceWidth;
     });
 
     ctx.lineTo(canvas.width, canvas.height / 2);
     ctx.stroke();
-    requestAnimationFrame(draw);
 }
 
-// set debug <p> element to show the lowest frequency
-const debug = document.getElementById('debug');
+function draw_blobs() {
+    game_state.blobs.forEach(blob => {
+        blob.draw();
+        blob.move();
+    })
+}
 
-function debugLowestFrequency() {
-    const dataArray = new Uint8Array(analyser.frequencyBinCount);
-    analyser.getByteFrequencyData(dataArray);
-    let loudest = 0;
-    let loudest_index = 0;
-    // get max value in array
-    dataArray.forEach((v, i) => {
-        if (v > loudest) {
-            loudest = v;
-            loudest_index = i;
-        }
-    });
-    debug.innerText = `
-    Lowest frequency: ${loudest_index}
-    Loudest frequency: ${loudest}
-    `;
-    volume = loudest;
-    freq = loudest_index;
-    requestAnimationFrame(debugLowestFrequency);
+game_state = {
+    blobs: []
+}
+
+function draw() {
+    draw_slope();
+    draw_blobs();
+    requestAnimationFrame(draw);
 }
 
 // get microphone input
@@ -83,29 +76,48 @@ navigator.mediaDevices.getUserMedia({audio: true, video: false})
         }
     );
 
-
+document.onclick = function (event) {
+    const x = event.x - 8
+    const y = event.y - 8
+    game_state.blobs.push(new Blob(x, y))
+    console.log(x,y)
+}
 
 class Blob {
-    constructor() {
-        this.x = 100;
-        this.y = 100;
-        this.width = 50;
-        this.height = 50;
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;
+        this.width = 5;
+        this.height = 5;
+        this.speed = 0;
     }
 
     draw() {
-        game_ctx.fillStyle = 'red';
-        game_ctx.beginPath();
-        game_ctx.arc(this.x, this.y, this.width, 0, 2 * Math.PI);
-        game_ctx.fill();
+        ctx.fillStyle = 'red';
+        ctx.beginPath();
+        ctx.arc(this.x-this.width/2, this.y-this.width/2, this.width, 0, 2 * Math.PI);
+        ctx.fill();
     }
 
     move(){
+        this.speed += 0.05
+        this.y += this.speed
+        for (let i = 0; i < game_state.line.length -1; i++){
+            const [x,y] = game_state.line[i];
+            const [nx, ny] = game_state.line[i+1];
+
+            if (this.x > x && this.x < nx){
+                const dx = this.x - x
+
+                if (this.y > y){
+                    this.y = y
+                    this.speed = -this.speed
+
+                }
+            }
+        }
     }
 }
 
 
-canvas.onclick(function () {
-    blob = new Blob();
-    blob.draw();
-})
+
